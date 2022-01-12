27a7533071ad022f9fd74715632a0165a00986f0
DevReev <munnalumpy@gmail.com>
2022-01-12 20:27:42 +0530

    Readme

--- components/Result.js
+++ components/Result.js
@@ -1,9 +1,9 @@
-import Thumbnail from "./ThumbNail.js";
+import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
-export default function Result({ results }) {
+export default function Results({ results }) {
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:flex flex-wrap justify-center rounded-md">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
